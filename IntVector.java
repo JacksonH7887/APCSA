0ef116b1da5dbc1828b64b72c5c3e82125eb88d5
JacksonH7887 <jacksonhaake25@amdg.rockhursths.edu>
2023-10-19 12:22:14 -0500

    commitOct19

---
+++ IntVector.java
@@ -0,0 +1,20 @@
+public class IntVector{
+
+    private final static int INITIAL_CAPACITY = 10;
+    private int[] arr = new int[INITIAL_CAPACITY];
+    private int size = 0;
+
+    //default constructor
+    public IntVector() { }
+
+    //overloaded constructor
+    public IntVector(int capacity){
+        if(capacity < 0){
+            throw new IllegalArgumentException();
+        }
+        arr = new int[capacity];
+    }
+
+    //returns function size
+    public int size(){return size;}
+}
---
+++ start.java
@@ -0,0 +1,19 @@
+public class start {
+    public static void main(String[] arg){
+        try{
+            IntVector test = new IntVector();
+
+            System.out.println("Size: " + test.size());
+    
+            for(int i = 0; i < test.size(); i++){
+    
+            }
+        }catch(IllegalArgumentException ex){
+            System.out.println("That's Illegal");
+        }catch(ArithmeticException ex){
+            System.out.println("Bad Math");
+        }catch(Exception ex){
+            System.out.println("Generic Exception");
+        }
+}
+}
