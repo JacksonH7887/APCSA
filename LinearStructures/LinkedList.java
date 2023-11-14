0acd550ff972479f82cf3a935b685e5840b93b85
JacksonH7887 <jacksonhaake25@amdg.rockhursths.edu>
2023-11-14 12:34:49 -0600

    linkedListStart

---
+++ LinearStructures/LinkedList.java
@@ -0,0 +1,98 @@
+package LinearStructures;
+
+public class LinkedList<E> {
+    private int size = 0;
+
+    private Node<E> head, tail;
+
+    public LinkedList(){
+
+    }
+
+    public LinkedList(E[] objects){
+        for(int i = 0; i < objects.length; i++){
+
+        }
+    }
+
+    public void add(E e){
+        add(size, e);
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    private void checkIndex(int index){
+        if(index < 0 || index >= size){
+            throw new IndexOutOfBoundsException("index " + index + " out of bounds");
+        }
+    }
+
+    public int indexOf(E e){
+        if(size == 0) return -1;
+
+        int index = 0;
+        Node<E> current = head;
+
+        do {
+            if(current.element == e) return index;
+            current = current.next;
+            index++;
+        } while(current != null);
+
+        return -1;
+    }
+
+    public void addFirst(E e){
+        Node<E> NewNode = new Node<>(e);
+        newNode.next = head;
+        head = newNode;
+        size++;
+
+        if(tail == null) tail = head;
+    }
+
+    public void addLast(E e){
+        Node<E> newNode = new Node<>(e);
+        if(tail == null) head = tail = newNode;
+        else{
+            tail.next = newNode;
+            tail = tail.next;
+        }
+    }
+
+    public void add(int index, E e){
+        if(index == 0) addFirst(e);
+        else if(index >= size) addLast(e);
+        else{
+            Node<E> current = head;
+            for(int i = 1; i < index; i++){
+                current = current.next;
+            }
+            Node<E> temp = current.next;
+            current.next = new Node<E>(e);
+            (current.next).next = temp;
+            size++;
+        }
+    }
+
+    public E getFirst(){
+        if(size == 0) return null;
+        return head.element;
+    }
+    
+    private static class Node<E>{
+        private E element;
+        private Node<E> next;
+
+        public Node(E element){
+            this.element = element;
+        }
+    }
+
+}
